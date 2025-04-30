c1e65946ea290d27883ddc57317e39985b5ba139
Alvar-234 <thealvar234@gmail.com>
2025-04-30 08:13:35 -0600

    Corecciones

--- src/PDVPapeleria/src/main/java/DAO/ProductoDAO.java
+++ src/PDVPapeleria/src/main/java/DAO/ProductoDAO.java
@@ -4,35 +4,15 @@
  */
 /**
  *
- * @author laura
+ * @author Alvaro
  */
 package DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import VO.ProductoVO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ProductoDAO {
+public interface ProductoDAO {
 
-    private final Connection connection;
-
-    public ProductoDAO() throws SQLException {
-        this.connection = DatabaseConnection.getInstance().getConnection();
-    }
-
-    /**
-     * Obtiene todos los productos no eliminados de la base de datos.
-     *
-     * @return Un ResultSet con los productos.
-     * @throws SQLException Si ocurre un error al ejecutar la consulta.
-     */
-    public ResultSet getProductos() throws SQLException {
-        String query = "SELECT idProducto, nombre, precioDeCompra, precioDeVenta, stock, categoria "
-                + "FROM producto "
-                + "WHERE is_deleted = FALSE "
-                + "ORDER BY nombre";
-        PreparedStatement statement = connection.prepareStatement(query);
-        return statement.executeQuery();
-    }
+    ResultSet getProductos() throws SQLException;
 }
